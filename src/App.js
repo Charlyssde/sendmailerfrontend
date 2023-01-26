4f43419f69332e1c149f625864fd22fa594e22a5
Carlos de Jesús Carrillo Ceballos <cjcarrillo@sefiplan.gob.mx>
2023-01-25 19:24:52 -0600

    UPDATE component text

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import {useState} from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, {Quill} from "react-quill";
+import ImageResize from 'quill-image-resize-module-react';
 import 'react-quill/dist/quill.snow.css'
 
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'align': [] }],
+    ['link', 'image'],
+    ['imageResize']
+];
 function App() {
-
-    const [convertedText, setConvertedText] = useState("Some default content");
+    Quill.register('modules/imageResize', ImageResize);
+    const [convertedText, setConvertedText] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
           <div>
               <ReactQuill
+                  modules={{toolbar : {container : toolbarOptions}, imageResize: {
+                          parchment: Quill.import('parchment'),
+                          modules: ['Resize', 'DisplaySize']
+                      }}}
                   theme='snow'
                   value={convertedText}
                   onChange={setConvertedText}
